91cd3bbcac1d3197a00dc7c3fa87c8a591c0c498
Manish Sajwan <manish.sajwan@protonmail.com>
2021-10-16 20:23:45 +0530

    states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="dog" breed="Wheaton Terrie" />
+      <SearchParams />
     </div>
   );
 };
