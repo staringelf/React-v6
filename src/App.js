3af5b213d1d4b78d286a599a3ff2b285c59491bd
Manish Sajwan <manish.sajwan@protonmail.com>
2021-10-16 18:47:26 +0530

    react and eslint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
   return (
-    <div> 
+    <div>
       <h1 id="my-brand">Adopt Me</h1>
       <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Sudo" animal="dog" breed="Wheaton Terrie" />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
