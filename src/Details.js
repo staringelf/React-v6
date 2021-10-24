dcd9e3ad1a75e0078cd777904ceab2e6d6a7107b
Manish Sajwan <manish.sajwan@protonmail.com>
2021-10-24 14:46:03 +0530

    loading state

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,11 @@
-import { Component } from 'react';
+import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-
   constructor() {
     super();
 
-    this.state = {loading: true};
+    this.state = { loading: true };
   }
 
   async componentDidMount() {
@@ -16,28 +15,35 @@ class Details extends Component {
 
     const json = await res.json();
 
-    this.setState(Object.assign({
-      loading: false
-      }, json.pets[0])
-    ); 
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
   }
 
-  render () {
+  render() {
+    if (this.state.loading === true) {
+      return <h2>loading...</h2>;
+    }
+
     const { animal, breed, city, state, description, name } = this.state;
     return (
       <div className="details">
         <div>
           <h1>{name}</h1>
-          <h2>{animal} - {breed} - {city} - {state}</h2>
+          <h2>
+            {animal} - {breed} - {city} - {state}
+          </h2>
           <button>Adopt {name}</button>
           <p>{description}</p>
         </div>
       </div>
-    )
-
+    );
   }
-
 }
-
 
 export default withRouter(Details);
