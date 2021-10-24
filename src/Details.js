a8e9ad70ff01a490e779b69642166f6bdf4b9a43
Manish Sajwan <manish.sajwan@protonmail.com>
2021-10-24 19:21:29 +0530

    class-properties

--- src/Details.js
+++ src/Details.js
@@ -2,11 +2,8 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
 
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
