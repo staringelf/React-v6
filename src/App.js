2e5e5652fc94b5584a73cfecaa907955124a7f38
Manish Sajwan <manish.sajwan@protonmail.com>
2021-10-24 14:38:18 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
 ReactDOM.render(
   <StrictMode>
-  <App />
-  </StrictMode>, document.getElementById("root"));
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/Details.js
@@ -0,0 +1,6 @@
+const Details = () => {
+  return  <h2>hi lololol!</h2>
+}
+
+export default Details;
+
