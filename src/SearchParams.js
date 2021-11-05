702ccd8af499654847d5ad84005d0fb813957856
Manish Sajwan <manish.sajwan@protonmail.com>
2021-11-05 20:01:34 +0530

    remove default location

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,7 +7,7 @@ import ThemeContext from "./ThemeContext";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState();
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
